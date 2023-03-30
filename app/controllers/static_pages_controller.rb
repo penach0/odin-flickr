be3cec718136d3011265e3370e78afee1740da87
penach0 <josemanuel.penacho@gmail.com>
2023-03-30 14:26:36 +0100

    Fetch photo ids from the response

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
   def index; end
 
   def show_photos
-    @response = Flickr.new.people.getPublicPhotos(user_id: params[:id])
+    flickr = Flickr.new
+    @response = flickr.people.getPublicPhotos(user_id: params[:id])
+    @photo_ids = @response.map(&:id)
   end
 end
