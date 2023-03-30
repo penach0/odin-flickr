1bc77045d1c77a020608b61cf4977ec7eb55c0a5
penach0 <josemanuel.penacho@gmail.com>
2023-03-30 15:28:54 +0100

    Display photos on the screen
    - Process the data from the photo id to the photo url:
      - photo id -> photo info (flickr.photos.getInfo method) ->
        photo url (Flick.url - provided by gem)
    - Use urls to display photos on img tags in the show_photos view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,9 @@ class StaticPagesController < ApplicationController
   def show_photos
     flickr = Flickr.new
     @response = flickr.people.getPublicPhotos(user_id: params[:id])
+    @username = flickr.people.getInfo(user_id: params[:id]).username
     @photo_ids = @response.map(&:id)
+    @photos_info = @photo_ids.map { |id| flickr.photos.getInfo(photo_id: id) }
+    @photo_urls = @photos_info.map { |info| Flickr.url(info) }
   end
 end
